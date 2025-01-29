0942e4f565da84812240bd82fd390acf5338d13c
Hadi Roshan <109845464+hadi999Roshan@users.noreply.github.com>
2025-01-29 16:23:59 +0330

    Added the Certificates and edited js code for navigation to sections

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let socialBtn = document.getElementById("social-tag");
 let bioBtn = document.getElementById("bio-tag");
-const scrollTo = [50, 300];
+let certificateBtn = document.getElementById("certificate-tag");
+
+const scrollTo = [130, 300];
 socialBtn.addEventListener("click", (e) => {
   changeDir(e);
 });
 bioBtn.addEventListener("click", (e) => {
+  changeDir(e);
+});
+certificateBtn.addEventListener("click", (e) => {
   changeDir(e);
 });
 
@@ -13,8 +18,10 @@ function changeDir(e) {
   let scrollPx;
   if (e.currentTarget.innerHTML == "Social Media Links") {
     scrollPx = 300;
+  } else if (e.currentTarget.innerHTML == "Biography") {
+    scrollPx = 130;
   } else {
-    scrollPx = 50;
+    scrollPx = 620;
   }
   window.scrollTo({
     top: scrollPx,
